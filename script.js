76897b0ed8863f587edb088d14e2bc67e0395d91
Darrin Duncan <darrin@darrinduncan.com>
2021-02-07 21:44:23 -0500

    Initialize The Javascript Slider Project

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function swapImage(element) {
+ let newThumb =  element.src;
+ document.getElementById("main-img").style.background = `url()`;
+document.getElementById("main-img").style.background = `url(${newThumb})`
+}
